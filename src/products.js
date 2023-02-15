2e12629b9493d9c2a91c69c13df3b20fe91861b1
Aditya <avermaav2002@gmail.com>
2023-02-15 21:04:37 +0530

    Intial commit

--- src/products.js
+++ src/products.js
@@ -7,6 +7,12 @@ import product6 from "./assets/products/6.webp";
 import product7 from "./assets/products/7.webp";
 import product8 from "./assets/products/8.webp";
 import product9 from "./assets/products/9.webp";
+import product10 from "./assets/products/10.webp";
+import product11 from "./assets/products/11.webp";
+import product12 from "./assets/products/12.webp";
+import product13 from "./assets/products/13.webp";
+import product14 from "./assets/products/14.webp";
+import product15 from "./assets/products/15.webp";
 
 export const PRODUCTS = [
   {
@@ -62,5 +68,41 @@ export const PRODUCTS = [
     productName: "The Crazy Women",
     price: 140.0,
     productImage: product9,
+  },
+  {
+    id: 10,
+    productName: "The Danger Village Bank",
+    price: 190.0,
+    productImage: product10,
+  },
+  {
+    id: 11,
+    productName: "The Haunted Man",
+    price: 540.0,
+    productImage: product11,
+  },
+  {
+    id: 12,
+    productName: "The Lotus bird",
+    price: 320.0,
+    productImage: product12,
+  },
+  {
+    id: 13,
+    productName: "Village Landscap",
+    price: 1999.0,
+    productImage: product13,
+  },
+  {
+    id: 14,
+    productName: "Corona And Women",
+    price: 1560.0,
+    productImage: product14,
+  },
+  {
+    id: 15,
+    productName: "The Approching Hand",
+    price: 180.0,
+    productImage: product15,
   },
 ];
